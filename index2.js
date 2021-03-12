56602e1ffe56f1e915c5ef7e58cdc46241a336de
Sunny Nguyen-Bui <snguyenbui@gmail.com>
2021-03-12 02:46:38 +0000

    Implementation using the request-promise-native library of iss_spotter

--- index2.js
+++ index2.js
@@ -8,9 +8,9 @@ nextISSTimesForMyLocation()
     console.log("It didn't work: ", error.message);
   });
 
-  const printPassTimes = (passTimes) => {
-    for (const time of passTimes.response) {
-      let passing = new Date(time.risetime * 1000);
-      console.log(`The next time the ISS will pass over is ${passing} for ${time.duration} seconds`);
-    }
-  };
+const printPassTimes = (passTimes) => {
+  for (const time of passTimes.response) {
+    let passing = new Date(time.risetime * 1000);
+    console.log(`The next time the ISS will pass over is ${passing} for ${time.duration} seconds`);
+  }
+};
--- iss_promised.js
+++ iss_promised.js
@@ -2,26 +2,26 @@ const request = require('request-promise-native');
 
 const fetchMyIP = () => {
   return request('https://api.ipify.org?format=json');
-}
+};
 
 const fetchCoordsByIP = (body) => {
-  const IP = JSON.parse(body).ip
+  const IP = JSON.parse(body).ip;
   return request(`https://freegeoip.app/json/${IP}`);
 };
 
 const fetchISSFlyOverTimes = (data) => {
   const coords = JSON.parse(data);
   return request(`http://api.open-noatify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`);
-}
+};
 
 const nextISSTimesForMyLocation = () => {
   return fetchMyIP()
-  .then(fetchCoordsByIP)
-  .then(fetchISSFlyOverTimes)
-  .then(body => {
-    const nextTimes = JSON.parse(body)
-    return nextTimes
-  });
-} 
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then(body => {
+      const nextTimes = JSON.parse(body);
+      return nextTimes;
+    });
+};
 
-module.exports = { nextISSTimesForMyLocation }
+module.exports = { nextISSTimesForMyLocation };
