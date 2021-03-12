fea87ba63b9e94645b89e0f50c1d5bf8d50d130b
Sunny Nguyen-Bui <snguyenbui@gmail.com>
2021-03-12 02:44:14 +0000

    Implementation using the request-promise-native library of iss_spotter

---
+++ index2.js
@@ -0,0 +1,16 @@
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+nextISSTimesForMyLocation()
+  .then((nextPasses) => {
+    printPassTimes(nextPasses);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
+
+  const printPassTimes = (passTimes) => {
+    for (const time of passTimes.response) {
+      let passing = new Date(time.risetime * 1000);
+      console.log(`The next time the ISS will pass over is ${passing} for ${time.duration} seconds`);
+    }
+  };
